dfcdc5e1c5b44523940bdbd3b555a6a74192bf35
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-04-02 11:04:49 +0700

    RectangleProgram

---
+++ src/RectangleProgram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RectangleProgram {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float are = width * height;
+        System.out.println("Area is : " + are);
+
+    }
+}
