43192db2649b9af5bf4b9a2711c8412fbcdc665e
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-04-02 11:00:26 +0700

    biến và kiểu dữ liệu

---
+++ src/VariablesAndDataTypes.java
@@ -0,0 +1,20 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        demo();
+    }
+    static void demo(){
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5f;
+        boolean b = true;
+        char c = 'a';
+
+        String s = "Ha Noi";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
